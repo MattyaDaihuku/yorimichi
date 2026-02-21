dff428ef734bf07fc9e84423f4d112e862179af0
LEON-72 <hr.leon72@gmail.com>
2026-02-21 15:23:37 +0900

    fix: 未ログイン時にヘッダーとサイドバーが重ならないように修正

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -33,7 +33,7 @@ export function Header({ title = "", className }: HeaderProps) {
       <header
         className={cn(
           "fixed inset-x-0 top-0 z-20 px-6 pt-[env(safe-area-inset-top)] h-[calc(4rem+env(safe-area-inset-top))] transition-colors duration-300",
-          user && "md:left-[72px]",
+          "md:left-[72px]",
           className || "bg-background"
         )}
       >
