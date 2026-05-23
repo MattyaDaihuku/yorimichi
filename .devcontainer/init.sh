760e9b9bbff6d543c3f5fce2ead016df660105b3
MattyaDaihuku <matcha@daifuku.dev>
2026-05-23 13:32:35 +0900

    fix

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -3,8 +3,15 @@
 set -xe
 
 sudo chown -R node:node /workspace
+sudo apt-get update
+sudo apt-get -y install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 git config --global --add safe.directory /workspace
-npm install --frozen-lockfile
-if [ -f src/prisma/schema.prisma ]; 
-    then npx prisma generate --schema=src/prisma/schema.prisma; 
+if [ -f /workspace/package-lock.json ]; then
+    npm ci
+else
+    npm install
+fi
+
+if [ -f /workspace/src/prisma/schema.prisma ]; then
+    npx prisma generate --schema=/workspace/src/prisma/schema.prisma
 fi
